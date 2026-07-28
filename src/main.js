2fe282aadefbcff2ae361befa7327841c54686d2
lilcoolbro-05 <nmandrakegabriel@gmail.com>
2026-07-28 03:10:58 +0200

    .

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import * as THREE from 'three';
+import './style.css'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from 'gsap'
+
 
 //Scene
 const scene = new THREE.Scene();
@@ -7,15 +11,93 @@ const scene = new THREE.Scene();
 const geometry =new THREE.SphereGeometry(3,64,64)
 const material = new THREE.MeshStandardMaterial({
     color: '#00ff83',
+    roughness: 0.4,
+    metalness: 0.5
 })
 const mesh = new THREE.Mesh(geometry,material)
 scene.add(mesh)
 
+//Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+//Light
+const light = new THREE.PointLight(0xffffff,100,100)
+light.position.set(0,10,10)
+light.intensity = 107
+scene.add(light)
+
 //Camera
-const camera = new THREE.PerspectiveCamera(45,800,600)
+const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 100)
+camera.position.z = 20
 scene.add(camera)
+
+
 //Renderer
 const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({canvas})
-renderer.setSize(800,600)
+renderer.setSize(sizes.width,sizes.height)
 renderer.render(scene,camera)
+renderer.setPixelRatio(2)
+
+
+//Controls
+const controls = new OrbitControls(camera,canvas)
+controls.enableDamping = true
+controls.enablePan = false
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 10
+
+//Resizing
+window.addEventListener('resize',()=>{
+    //Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    //Update camera
+    camera.aspect = sizes.width/sizes.height
+    camera.updateProjectionMatrix()
+
+    //Update renderer
+    renderer.setSize(sizes.width,sizes.height)
+})
+
+
+const loop = () => {
+    controls.update()
+    renderer.render(scene,camera)
+    window.requestAnimationFrame(loop)
+}
+loop()
+
+//Timeline magic
+const tl = gsap.timeline({defaults:{duration:1}})
+tl.fromTo(mesh.scale,{z:0,x:0,y:0},{z:1,x:1,y:1})
+tl.fromTo('nav',{y:'-100%'},{y:'0%'})
+tl.fromTo('.title',{opacity:0},{opacity:1})
+
+//Mouse animation color
+let mouseDown = false
+let rgb = []
+window.addEventListener('mousedown',()=> (mouseDown = true))
+window.addEventListener('mouseup',()=> (mouseDown = false))
+
+window.addEventListener('mousemove',(e)=>{
+    if(mouseDown){
+        rgb = [
+            Math.round((e.pageX/sizes.width)*255),
+            Math.round((e.pageY/sizes.height)*255),
+            150
+        ]
+        //Animate
+        let newColor = new THREE.Color(`rgb(${rgb.join(',')})`)
+        gsap.to(mesh.material.color,{
+            r:newColor.r,
+            g:newColor.g,
+            b:newColor.b
+        })
+    }
+    })
